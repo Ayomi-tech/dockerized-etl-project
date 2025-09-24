af5b69378af6b7519fe1293bdaed3a8812d52dd7
Ayodeji <aayo.agunbiade@gmail.com>
2025-09-24 10:07:07 +0200

    ETL Load file

--- etl-docker-project/etl/load.py
+++ etl-docker-project/etl/load.py
@@ -8,7 +8,7 @@ def load(df):
     engine = create_engine("postgresql+psycopg2://etl_user:etl_pass@db:5432/etl_db") #SQLAlchemmy engine (Postgres)
 
 
-    # This write Dataframe to SQL and name it 'financial' 
+    # This write Dataframe to SQL and rename 'df' to 'financial' 
     df.to_sql("financial", engine, if_exists="replace", index=False)
 
     print(f"Loaded {len(df)} rows into table 'financial")
